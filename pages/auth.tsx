4e987151ea2da9e54b817bcfa04c5675f773a28e
System Administrator <root@MacBook-Pro-de-bonjour.local>
2024-01-03 20:24:56 +0100

    Ajout de nouveaux composants pour la mise en place de la NavBar.
    Dans le cadre de cette modification, les changements suivants ont été apportés :
    - Ajout des fichiers NavBar.tsx; NavBarItem.tsx; MobileMenu.tsx; AccountMenu.tsx,  pour les nouveaux composants.
    - Intégration des styles CSS nécessaires pour la mise en forme de la NavBar.
    - Mise à jour du fichier auth.tsx; index.tsx; Input.tsx  pour inclure les nouveaux composants.
    
    Ces modifications visent à améliorer la structure et l'apparence de la NavBar, contribuant ainsi à une meilleure expérience utilisateur.
    
    Signé par: Martinet Rémy remymartinet.contact@gmail.com

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react"
 import { FcGoogle } from 'react-icons/fc'
 import { FaGithub } from 'react-icons/fa'
 
-import Input from './components/Input'
+import Input from '../components/Input'
 const Auth = () => {
 
 
